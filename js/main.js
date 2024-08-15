f2dd7befd091c0c514b3e0b1865a37699ffa773a
tarek7salamaa <tarek7salamaa@gmail.com>
2024-08-15 16:57:27 +0300

    dark mode inhansement

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 // Dark Mode
 $(document).ready(function () {
+  // When Page Reload Check Dark-Mode Value
+  if (localStorage.getItem("dark-mode") === "on") {
+    $("body").addClass("dark-mode");
+    $("nav, .landing, .articls, footer").addClass("dark-mode");
+  } else {
+    $("body").removeClass("dark-mode");
+    $("nav, .landing, .articls, footer").removeClass("dark-mode");
+  }
+
+  // When Click To Swith Mode
   $(".switch-mode").on("click", function () {
-    $("body").toggleClass("dark-mode");
-    $("nav, .landing, .articls, footer").toggleClass("dark-mode");
+    if (localStorage.getItem("dark-mode") === "off") {
+      window.localStorage.setItem("dark-mode", "on");
+      $("body").addClass("dark-mode");
+      $("nav, .landing, .articls, footer").addClass("dark-mode");
+    } else {
+      window.localStorage.setItem("dark-mode", "off");
+      $("body").removeClass("dark-mode");
+      $("nav, .landing, .articls, footer").removeClass("dark-mode");
+    }
   });
 });
