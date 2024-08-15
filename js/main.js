e72c7c11034c8314e572cb66bcc3c42ef1cdea2c
tarek7salamaa <tarek7salamaa@gmail.com>
2024-08-15 17:30:20 +0300

    Add Dark mode in all screen

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 // Dark Mode
+if (localStorage.getItem("dark-mode") === "on") {
+  document.body.classList.add("dark-mode");
+}
+
+document.body.style.opacity = 1;
+
 $(document).ready(function () {
   // When Page Reload Check Dark-Mode Value
   if (localStorage.getItem("dark-mode") === "on") {
     $("body").addClass("dark-mode");
-    $("nav, .landing, .articls, footer").addClass("dark-mode");
+    $("nav, .landing, .articls, footer, .art").addClass("dark-mode");
   } else {
     $("body").removeClass("dark-mode");
-    $("nav, .landing, .articls, footer").removeClass("dark-mode");
+    $("nav, .landing, .articls, footer, .art").removeClass("dark-mode");
   }
 
   // When Click To Swith Mode
@@ -14,11 +20,11 @@ $(document).ready(function () {
     if (localStorage.getItem("dark-mode") === "off") {
       window.localStorage.setItem("dark-mode", "on");
       $("body").addClass("dark-mode");
-      $("nav, .landing, .articls, footer").addClass("dark-mode");
+      $("nav, .landing, .articls, footer, .art").addClass("dark-mode");
     } else {
       window.localStorage.setItem("dark-mode", "off");
       $("body").removeClass("dark-mode");
-      $("nav, .landing, .articls, footer").removeClass("dark-mode");
+      $("nav, .landing, .articls, footer, .art").removeClass("dark-mode");
     }
   });
 });
